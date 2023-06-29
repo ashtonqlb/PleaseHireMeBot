af9c4fb4bdef72f51c3c685400177837128ac5e4
Ashton Bennet <ashtonquinn@disroot.org>
2023-06-29 15:52:59 -0400

    added cow.py

---
+++ extensions/cow.py
@@ -0,0 +1,60 @@
+from interactions import slash_command, slash_option, SlashContext, Extension, OptionType
+
+def message():
+    return slash_option(name="message", description="Add a message", required=False, opt_type=OptionType.STRING)
+
+def eyes():
+    return slash_option(name="eyes", description="Choose custom eyes", required=False, opt_type=OptionType.STRING)
+
+def tongue():
+    return slash_option(name="tongue", description="Choose a custom tongue", required=False, opt_type=OptionType.STRING)
+
+class cow(Extension):
+    @slash_command(name = "cow", description = "")
+    async def cow(self, ctx: SlashContext):
+        ctx.send()
+    
+    cow.subcommand(sub_cmd_name="say", sub_cmd_description="GNU cowsay")
+    @message()
+    @eyes()
+    @tongue()
+    
+    async def cowsay(self, 
+                     ctx: SlashContext, 
+                     message ="moo",
+                     eyes ="oo",
+                     tongue = "__"
+                     ):
+        output = f"""```
+        {message}
+         \   ^__^
+          \  ({eyes})\_______
+             ({tongue})\       )\/
+                 ||----w |
+                 ||     ||```
+        """.format(message=message)
+        await ctx.respond(output)
+        
+    cow.subcommand(sub_cmd_name="think", sub_cmd_description="GNU cowthink")
+    @message()
+    @eyes()
+    @tongue()
+    
+    async def cowthink(self, 
+                       ctx: SlashContext, 
+                       message ="moo",
+                       eyes ="oo",
+                       tongue = "__"
+                       ):
+        output = f"""```
+        {message}
+         \   ^__^
+          \  ({eyes})\_______
+             ({tongue})\       )\/
+                 ||----w |
+                 ||     ||```
+        """.format(message=message)
+        await ctx.respond(output)
+
+def setup(bot):
+    cow(bot)
--- extensions/fortune.py
+++ extensions/fortune.py
@@ -3,37 +3,37 @@ import re
 from interactions import slash_command, SlashContext, Extension
 
 class fortune(Extension):   
-    def _read_fortunes(self, fortune_file):
+    def readFortunes(self, fortune_file):
         with open(fortune_file, mode='r', encoding='utf-8') as f:
             contents = f.read()
 
         lines = [line.rstrip() for line in contents.split('\n')]
-        delim = re.compile(r'^%$')
+        delimiter = re.compile(r'^%$')
 
         fortunes = []
         temp = []
 
-        def add_to_list(buf):
+        def add(buf):
             parsed_fortune = '\n'.join(buf)
             if parsed_fortune.strip():
                 fortunes.append(parsed_fortune)
 
         for line in lines:
-            if delim.match(line):
-                add_to_list(temp)
+            if delimiter.match(line):
+                add(temp)
                 temp = []
                 continue
 
             temp.append(line)
 
         if temp:
-            add_to_list(temp)
+            add(temp)
 
         return fortunes
     
     @slash_command(name="fortune", description="GNU fortune")
     async def fortune(self, ctx: SlashContext):
-        fortunes = list(self._read_fortunes("extensions/assets/fortune/fortunes2"))
+        fortunes = list(self.readFortunes("extensions/assets/fortune/fortunes2"))
         message = fortunes[random.randint(0, len(fortunes) - 1)]
         await ctx.send(message)
         
